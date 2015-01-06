a425845e4529ff41776f623be1714770d2bad10e
Becheler <arnaud.becheler@laposte.net>
2015-01-06 17:54:17 +0100

    Add comentaries and restructure them, for better understanding

--- Initialisation2.R
+++ Initialisation2.R
@@ -6,51 +6,71 @@ wd="/home/dupas/GraphPOP/" # fixe
 wd="/media/1To/IRD/ARTICLES/Dupas/NicheCoal/Graph_Pop" # portable
 wd="/media/dupas/1To/IRD/ARTICLES/Dupas/NicheCoal/Graph_Pop" # fixe
 wd="/home/arno/These/GraphPOP" # portable arno
-
-# test simulation backward
 setwd(wd)
 source("graphPOP_0.114.R")
-# on va mettre des données environnementales de température et précipitations annuelles dans une data frame
-#Data2 <- data.frame(BIO1=c(200,120,300,400),BIO12=c(1000,350,2000,2900)) 
+
+########### Parameters initialisation  ########### >>>>>>
+
+# Environmental data of temperature and precipitations are put in a dataframe
+# Data2 <- data.frame(BIO1=c(200,120,300,400),BIO12=c(1000,350,2000,2900)) 
 Data2 <- data.frame(BIO1=c(300,120,120,400),BIO12=c(2000,350,350,2900)) 
-# on va fabriquer une raster stack avec deux couches correspondans aux variables environementales de la data frame
+# Make raster stack with two layers according to the environmental variables of the dataframe
 rasterStack <- stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1),"BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
-# for each demographic varaibles (r and K)
-# we have a niche function (reaction norm)
-# the shapes of the reaction =norms are given by the variables shapeK and shaper
-# the parameters of the reaction norm are given by  and pr
-shapesK=c(BIO1="conquadraticskewed",BIO12="conquadraticskewed")
+
+###### Genetic parameters :
 N=2
 mutation_rate=1E-4
 initial_genetic_value=200
-pK = matrix(c(100,500,300,0,N,N,300,3000,2500,0,N,N),nrow=6,ncol=2,dimnames=list(c("Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
+
+##### Niche Function :
+
+# Shapes of the reaction norms for demographic variables (r and K) :
+shapesK=c(BIO1="conquadraticskewed",BIO12="conquadraticskewed")
 shapesr=c(BIO1="conquadraticskewed",BIO12="conquadraticskewed")
+# Parameters of the reaction norm are given by pK and pr
+pK = matrix(c(100,500,300,0,N,N,300,3000,2500,0,N,N),nrow=6,ncol=2,dimnames=list(c("Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
 pr = matrix(c(100,500,300,0,N,N,300,3000,2500,0,N,N),nrow=6,ncol=2,dimnames=list(c("Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
-# we also have a shape of the dispersion function given here by the variable "shapeDisp"
-# and dispersion parameters of the dispersion function
-pDisp = c(1/19,1)
+
+##### Dispersion Function :
+
+# Shape of the dispersion function :
 shapeDisp="fat_tail1"
-# we test coalescence simulation fonction from invented coordinates values
-# to have individuals sampled in areas where they are likely to be present
-# we use the carrying capacity map
+# Dispersion parameters of the dispersion function
+pDisp = c(1/19,1)
+
+######################### end of parameters initialisation <<<<<<
+
+
+######################### Coalescence Simulation Test >>>>>>>
+
+##### Get the carrying capacity map :
 rasK=rasterStack;values(rasK)= as.matrix(ReactNorm(values(rasterStack),pK,shapesK)[,"Y"])
-geneticData = CreateGenetArray(rasK, 20,200,Option="full_1col_diploid")# the genetic data is meaning less (only one genotype), 
-                                                              # but genetic data are not used. They will be modified according to the simulated coalescent
-                                                              # we create as many individuals (lines) as K for each cell of the map 
-                                                              # then we sample a few individuals for the coalescent
+
+###### Create genetic data 
+# the genetic data is meaning less (only one genotype),
+# but genetic data are not used. They will be modified according to the simulated coalescent
+# we create as many individuals (lines) as K for each cell of the map 
+# then we sample a few individuals for the coalescent
+geneticData = CreateGenetArray(rasK, 20,200,Option="full_1col_diploid")          
 geneticData = CreateGenetArray(rasK, 20,sample((80:120)*2,6*20,replace=TRUE),Option="sample_1col_diploid",nind=3)
 dim(geneticData)
+
+##### Get the migration Matrix
 migrationMatrix(rasK,shapeDisp,pDisp)
-system.time(coalescent_simulated <- simul_coalescent(geneticData,rasterStack,pK,pr,shapesK,shapesr,shapeDisp,pDisp,mutation_rate=1E-1))# we simulate a coalescent
+
+##### Simulate the coalescent
+system.time(coalescent_simulated <- simul_coalescent(geneticData,rasterStack,pK,pr,shapesK,shapesr,shapeDisp,pDisp,mutation_rate=1E-1))
 coalescent_simulated
 apetree <- coalescent_2_newick(coalescent_simulated$coalescent)
+
+##### Plot the coalescent
 plot_coalescent(coalescent_simulated$coalescent,with_landscape=TRUE,rasK=rasK,legend_right_move=-.3)
 plot_coalescent(coalescent_simulated$coalescent,rasK=rasK,legend_right_move=-.1)
-# we plot the coalescent
 
+######################### end of Coalescence Simulation Test <<<<<<<<
 
-# Check 
  
+####### PSEUDO TRASH >>>>>>>>>>
 
 ###
 setwd(wd)

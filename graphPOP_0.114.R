4e5e978e9a909df2d7e8fcad30f6fff2e89a5a9a
Becheler <arnaud.becheler@laposte.net>
2015-01-07 13:00:28 +0100

    Modify the function CreateGenetArray adding comments and erasing per default arguments  settings

--- graphPOP_0.114.R
+++ graphPOP_0.114.R
@@ -424,34 +424,36 @@ geneticDist <- function(commute_time, popSize){
 # dataSize = data that contains number of individuals in each cell
 # nb_locus = number of locus wished
 # initial_locus_value = genetic information for the first generation (parents)
-# option =  sample/full_1col/2col_haploid/diploid
+# Option =  sample/full_1col/2col_haploid/diploid
 #           sample : nind individuals are sampled from the full population
 #           full : number of individuals per cell equals carrying capacity
 #           1col/2col : for diploids, one columns or 2 columns representation
 #           haploid or diploid : haploid or diploid 
 # nind = number of individuals sampled from the full carrying capacity population
 
-CreateGenetArray <- function(rasK, nb_locus, initial_locus_value,Option="sample_1_col_diploid",nind=4)
+CreateGenetArray <- function(rasK, nb_locus, initial_locus_value, Option, nind)
 {
-  #Get coords for each cell
-  coords = xyFromCell(rasK, 1:length(values(rasK[[1]])), spatial=FALSE)
+  # Get coords for each cell
+  coords = xyFromCell(object=rasK, cell=1:length(values(rasK[[1]])), spatial=FALSE)
+  # Get a vector giving the number of the deme, repeted as many times as sampled individuals within (ex : 111144 )
   repet = switch(Option,
-                 sample_1col_diploid = sort(rep(sample(rep(1:ncell(rasK),round(values(rasK))),nind),2),replace=TRUE), 
-                 sample_2col_diploid = sample(rep(1:length(rasK),round(values(rasK))),nind,replace=TRUE), 
+                 sample_1col_diploid = sort(rep(sample(rep(1:ncell(rasK),round(values(rasK))),nind,replace=TRUE),2)),
+                 sample_2col_diploid = sample(rep(1:length(rasK),round(values(rasK))),nind,replace=TRUE),
                  sample_haploid = sample(rep(1:ncell(rasK),round(values(rasK))),nind,replace=TRUE),
-                 full_1col_diploid = rep(1:length(rasK),round(values(rasK))*2), 
-                 full_2col_diploid = rep(1:length(rasK),round(values(rasK))), 
-                 full_haploid = rep(1:length(rasK),round(values(rasK))),                 
+                 full_1col_diploid = rep(1:length(rasK),round(values(rasK))*2),
+                 full_2col_diploid = rep(1:length(rasK),round(values(rasK))),
+                 full_haploid = rep(1:length(rasK),round(values(rasK))),            
   )
-  geneticData <- as.data.frame(coords[repet,]) ; geneticData[,"Cell_numbers"] <- repet
+  # Make a data frame of genetic values :
   genes = switch(Option,
                  sample_1col_diploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus))), 
-                 sample_2col_diploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus*2))), 
+                 sample_2col_diploid  = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus*2))), 
                  sample_haploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus))),
                  full_1col_diploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus))), 
                  full_2col_diploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus*2))), 
                  full_haploid = as.data.frame(array(initial_locus_value,dim=c(length(repet),nb_locus))),                 
   )
+  # determine names of column, like "Locus 1","Locus 2" for 1_col_diploid and haploid, or "Locus_1.1", "Locus_1.2" for 2_col_diploid 
   colnames(genes) = switch(Option,
                            sample_1col_diploid = paste("Locus",1:nb_locus, sep=""), 
                            sample_2col_diploid = paste("Locus",sort(rep(1:nb_locus,2)),".", 1:2, sep=""), 
@@ -460,6 +462,9 @@ CreateGenetArray <- function(rasK, nb_locus, initial_locus_value,Option="sample_
                            full_2col_diploid = paste("Locus",sort(rep(1:nb_locus,2)),".", 1:2, sep=""), 
                            full_haploid = paste("Locus",1:nb_locus, sep=""),                 
   )
+  # Merge sampling, geographic, genetic informations
+  geneticData <- as.data.frame(coords[repet,])
+  geneticData[,"Cell_numbers"] <- repet
   geneticData <- cbind(geneticData,genes) # add locus to geneticData 
   geneticData
 }

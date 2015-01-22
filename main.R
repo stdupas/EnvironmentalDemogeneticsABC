9b9373b66c8eb8176ca313c6d665d29a7d52c856
Arnaud Becheler <arnaudbecheler@laposte.net>
2015-01-22 11:05:39 +0100

    Restructuring the main code, now using ParamList

--- main.R
+++ main.R
@@ -15,6 +15,7 @@ source("NicheFunctions.R")
 source("DispersionFunctions.R")
 source("MutationFunctions.R")
 source("CoalescentFunctions.R")
+source("PriorFunctions.R")
 
 ### Sourcing Libraries
 library(raster)
@@ -22,49 +23,40 @@ library(ape)
 library(stringr)
 library(lattice)
 
-########### Parameters initialisation  (a priori useless when asking to user...) ########### >>>>>>
+########### Parameters initialisation  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 ###### Environmental data of temperature and precipitations
-
 # Data2 <- data.frame(BIO1=c(200,120,300,400),BIO12=c(1000,350,2000,2900)) 
 Data2 <- data.frame(BIO1=c(300,120,120,400),BIO12=c(2000,350,350,2900)) 
 # Make raster stack with two layers according to the environmental variables of the dataframe
-rasterStack <- stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1),"BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
+rasterStack <- stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1),
+                          "BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
 
 ###### Genetic parameters :
 N=1.5
 mutation_rate=1E-4
-initial_genetic_value=200
 
-##### Niche Function :
+###### Asking List to the user
+ParamList <- askListOfParameters(rasterStack=rasterStack, nb_simulations=10)
+# Or load it from working directory
+ParamList <- load("ParamList.R")
 
-# Concave quadratic skewed distribution parameters
-# Shapes of the reaction norms for demographic variables (r and K) :
-shapesK=c(BIO1="conquadraticskewed",BIO12="conquadraticskewed")
-shapesr=c(BIO1="linearPositive",BIO12="conquadraticskewed")
-# Parameters of the reaction norm are given by pK and pr
-pK = matrix(c(100,500,300,0,N,N,300,3000,2500,0,N,N),nrow=6,ncol=2,dimnames=list(c("Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
-#pr = matrix(c(100,500,300,0,N,N,300,3000,2500,0,N,N),nrow=6,ncol=2,dimnames=list(c("Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
-pr = matrix(c(100,0.01,NA,NA,NA,NA,NA,NA,NA,NA,300,3000,2500,0,N,N),nrow=8,ncol=2,dimnames=list(c("X0","slope","Xmin","Xmax","Xopt","Yxmin","Yxmax","Yopt"),c("BIO1","BIO12")))
-
-# Linear model + constant model parameters
-shapesK=c(BIO1="linearPositive",BIO12="linearPositive")
-shapesr=c(BIO1="constant",BIO12="constant")
-pK = matrix(c(100,0.01,300,0.001),nrow=2,ncol=2,dimnames=list(c("X0","slope"),c("BIO1","BIO12")))
-pr = matrix(c(N,N),nrow=1,ncol=2,dimnames=list(c("Y"),c("BIO1","BIO12")))
-
-##### Dispersion Function :
-
-# Shape of the dispersion function :
-shapeDisp="fat_tail1"
-# Dispersion parameters of the dispersion function
-pDisp = c(alpha=1/19,beta=1)
-
-######################### end of parameters initialisation <<<<<<
+########## end of parameters initialisation <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 
-######################### Using Functions :
+######################### Using Functions >>>>>
+
 ##### Get the carrying capacity map :
-rasK=rasterStack
+ParamList[["Niche"]]
+response <-nicheFunctionForRasterStack(functionList=list(conquadraticSkewed1, linearPositiveTwoParameters), 
+                                       rasterStack=stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1), 
+                                                              "BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
+                                       , args=list(list(Xmin=0, Xmax=10, Xopt=5, Yopt=1),list(X0=0, slope=1/2)))
+
+response <-nicheFunctionForRasterStack(functionList=list(conquadraticSkewed1, linearPositiveTwoParameters), 
+                            rasterStack=stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1), 
+                                                   "BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
+                            , args=list(list(Xmin=0, Xmax=10, Xopt=5, Yopt=1),list(X0=0, slope=1/2)))
+
 values(rasK)= as.matrix(ReactNorm(X=values(rasterStack),p=pK,shapes=shapesK)[,"Y"])

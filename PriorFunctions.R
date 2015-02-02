d8cab7c11d3fca8a2d96dc68c4fde0e23ca382de
Arnaud Becheler <arnaudbecheler@laposte.net>
2015-02-02 10:31:20 +0100

    adding few prior functions

--- PriorFunctions.R
+++ PriorFunctions.R
@@ -5,5 +5,46 @@ uniform <- function(n, min, max){
   #   n: number of observations
   #   min: lower limit of the distribution
   #   max: upper limit of the distribution
-  return(runif(n,min,max))
+  #
+  # Returns:
+  #   a random sample
+  return(runif(n, min, max))
 }
+
+beta <- function(n, shape1, shape2){
+  # random generation for the beta distribution 
+  # 
+  # Args:
+  #   n: number of observations
+  #   shape1: non-negative parameter of the Beta distribution
+  #   shape2: non-negative parameter of the Beta distribution
+  #
+  # Returns:
+  #   A random sample
+  return(rbeta(n, shape1, shap2))
+}
+
+normal <- function(n, mean, sd){
+  # random generation for the normal distribution 
+  # 
+  # Args:
+  #   n: number of observations
+  #   mean: vector of means
+  #   sd: vector of standard deviations
+  #
+  # Returns:
+  #   A random sample
+  return(rnorm(n, mean, sd))
+}
+
+gamma <- function(n, shape){
+  # random generation for the gamma distribution 
+  # 
+  # Args:
+  #   n: number of observations
+  #   shape: shape parameter. Must be positive.
+  #
+  # Returns:
+  #   A random sample
+}
+

db5b1fc4429da8647942afe727957a5c52b6aea9
Arnaud Becheler <arnaudbecheler@laposte.net>
2015-02-04 13:52:09 +0100

    adding a line to simulate genetic Data

--- main.R
+++ main.R
@@ -33,12 +33,6 @@ Data2 <- data.frame(BIO1=c(300,120,120,400),BIO12=c(2000,350,350,2900))
 rasterStack <- stack(list("BIO1"=raster(matrix(Data2$BIO1,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1),
                           "BIO12"=raster(matrix(Data2$BIO12,nrow=1,ncol=4),xmn=0,xmx=4,ymn=0,ymx=1)))
 
-
-# where are the sampled data ?
-localizationData <- c(1,4,2,2,1,1,2,3);
-names(localizationData)=1:length(localizationData)
-
-
 ###### Asking List to the user
 # ParamList <- askListOfParameters(rasterStack=rasterStack, nb_simulations=10)
 # save(ParamList, file = "ParamList.RData")
@@ -80,5 +74,15 @@ migrationMatrix <- migrationRateMatrix(kernelMatrix)
 transitionBackward <- transitionMatrixBackward(r=values(r), K=values(K), migration= migrationMatrix)
 transitionForward <- transitionMatrixForward(r=values(r), K=values(K), migration= migrationMatrix, meth="non_overlap")
 
+###################### /!\ /!\ /!\ ######
+GeneticData <- CreateGenetArray(rasK=round(K), nb_locus=3, initial_locus_value = 200, Option ="sample_haploid", nind = 10)
+###################### /!\ /!\ /!\ ######
+
+# where are the sampled data ?
+localizationData <- cellFromXY(object = K, xy = GeneticData[, c("x", "y")])
+names(localizationData)=1:length(localizationData)
+
 # launch the coalescent
 coalescentList <- simul_coalescent_only(tipDemes=localizationData,transitionForward=transitionForward, transitionBackward=transitionBackward, K=K)
+
+

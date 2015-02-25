3fee42dbbc565f0647f7c522bb3a5572cbbc1400
dupas <legs@tmp-Dupas-Latitude-E6330.(none)>
2015-02-25 20:27:45 +0100

    Wew forward fonctions at the sacle of the deme, are now called in anew main named 'mainForward_Demes.R'

--- forwardModels.R
+++ forwardModels.R
@@ -1,3 +1,42 @@
+aggregation <- function(df,BY,methodes)
+{
+  # Function to aggregate lines of a data.frame
+  # arguments
+  # df: data.frame to aggregate
+  # BY: columns to use for aggregation of lines, lines having the same value in all the columns will be aggregated
+  # value
+  # a data.frame aggregated. Numeric and integer columns are given mean value of aggregated lines. Character and other class
+  # of vectors are given collapsed character string value, or first value if all the same
+  
+  df$MergeBY <- NA
+  for (ligne in 1:nrow(df)){df$MergeBY[ligne]=paste(df[ligne,BY],collapse="_")}
+  df <- df[order(df[,"MergeBY"]),];rownames(df)=1:nrow(df)
+  df_return=df[1,];df_return=df_return[-1,]
+  ligne=1
+  methodes = append(methodes,"Name")
+  while (ligne <= dim(df)[1])
+  {
+    print(ligne)
+    lignes = which(df[,"MergeBY"]==df[ligne,"MergeBY"])
+    if (length(lignes)==0) {ligne=ligne+1} else{
+      sub_df <- df[lignes,]
+      if (length(lignes)>1) {df_return <- rbind(df_return,sub_df[1,])
+                             for (colonne in 1:dim(sub_df)[2]) {
+                               df_return[nrow(df_return),colonne] = switch(methodes[colonne],
+                                                                           Mean = mean(na.omit(sub_df[,colonne])),
+                                                                           Sum =  sum(na.omit(sub_df[,colonne])),
+                                                                           Name = na.omit(sub_df[,colonne])[1],
+                                                                           Paste = paste(sub_df[,colonne],collapse="_")
+                                                                           )
+                             } 
+      }
+      else {df_return <- rbind(df_return,df[lignes,])}
+      ligne=ligne+length(lignes)
+    }
+  }
+  return(df_return[,-ncol(df_return)])
+}
+
 forwardMigrationRateMatrixFromKernel <- function(dispersionKernel){
   # Normalizes a matrix of dispersion kernel between cells so that individuals bounce on the frontier.
   #
@@ -110,15 +149,16 @@ reprMigr <- function(tipDemes,migrationMatrix,r,generationTime,generationTimeRel
   #          generationTimeRelativeSD)
   #
 
-  rownames(tipDemes)<-tipDemes$individualNb
+  #rownames(tipDemes)<-tipDemes$individualNb
   repMig = r*migrationMatrix
   # caculating absolute reproduction migration matrix with expected 
   # offspring size in column for each deme of the tip in line
-  trTip <- subset(repMig,subset=(1:nrow(repMig)==tipDemes$demeNb))
+  #trTip <- subset(repMig,subset=(1:nrow(repMig)==tipDemes$demeNb))
+  trTip <- repMig[tipDemes$demeNb,]; if(class(trTip)=="numeric") {trTip <- as.matrix(t(trTip))}
   # generating number of offspring of each tip (line) in each deme (column)
   individualNb <- matrix( rpois(prod(dim(trTip)),trTip),nrow=nrow(trTip),ncol=ncol(trTip))
   # 
-  offsTipDemes <- tipDemes[as.character(rep(tipDemes$individualNb,rowSums(individualNb))),]
+  offsTipDemes <- tipDemes[as.character(rep(rownames(tipDemes),rowSums(individualNb))),]
   #offsTipDemes$birthDate <- offsTipDemes$birthDate + rnorm(nrow(offsTipDemes),generationTime[offsTipDemes$demeNb],generationTimeRelativeSD*generationTime[offsTipDemes$demeNb])
   offsTipDemes$birthDate <- as.character(offsTipDemes$birthDate + rnorm(nrow(offsTipDemes),generationTime,generationTimeRelativeSD*generationTime))
   offsTipDemes$demeNb <- rep(rep(1:length(r),nrow(individualNb)),as.vector(t(individualNb)))
@@ -178,4 +218,51 @@ generateIndividualSerie <- function(release,r_serie,K_serie,migrationMatrix, sta
     individuals <- rbind(individuals,reprMigrCompet(individuals[which(individuals$birthDate==Date),],absoluteForwardTransitionMatrix,generationTime,generationTimeSD))
   }
 individuals
+}
+
+demeReprodMigr <- function()
+{
+  
+}
+
+generateDemeSizeSerie <- function(release,rasterStack,dispersionFunction,dispersionParameters,nicheFunctionList,nicheParametersList,generationTimeParameters)
+{
+  kernelMatrix <- dispersionFunctionForRasterLayer(dispersionFunction=dispersionFunction, 
+                                                   args=dispersionParameters)
+  migrationMatrix <- forwardMigrationRateMatrixFromKernel(kernelMatrix)
+  rm(kernelMatrix)
+  generationTime <- fwParamList$generationTime$mean$values[simulation]
+  generationTimeSD <- fwParamList$generationTime$SD$values[simulation]*fwParamList$generationTime$mean$values[simulation]
+  release$demeNb <- cellFromXY(object = rasterStack, xy = release[, c("x", "y")])
+  release$demeSize <- 1
+  Dates = as.Date(min(release$birthDate):max(release$birthDate),origin="1970-01-01")
+  demeSizes <- array(0,dim=c(nrow(EnvData),length(Dates)),dimnames = list(1:nrow(EnvData),as.character(Dates)))
+  release <- aggregation(release,BY=c("birthDate","demeNb"),methodes=c("Paste","Mean","Mean","Name","Name","Sum"))
+  for (i in rownames(release))
+    {demeSizes[release[i,"demeNb"],as.character(release[i,"birthDate"])] <- release[i,"demeSize"]}
+  for (Date in as.Date(as.Date(fwParamList$startingDate):as.Date(fwParamList$stoppingDate),origin="1970-01-01"))
+  {
+    values(rasterStack) <- EnvData[,as.character(Date),]
+    K <- values(nicheFunctionForRasterStack(functionList = getFunctionListNiche(ParamList = fwParamList, sublist="NicheK"), 
+                                            rasterStack = rasterStack,
+                                            args = getArgsListNiche(simulation = simulation, ParamList = fwParamList, sublist="NicheK")
+                                            )
+                )
+    demeSizes[,as.character(Date)] <- (demeSizes[,as.character(Date)]>K)*K + round((demeSizes[,as.character(Date)]<=K)*demeSizes[,as.character(Date)])
+    r <- values(nicheFunctionForRasterStack(functionList = getFunctionListNiche(ParamList = fwParamList, sublist="NicheR"), 
+                                            rasterStack = rasterStack,
+                                            args = getArgsListNiche(simulation = simulation, ParamList = fwParamList, sublist="NicheR")
+                                            )
+                )
+    descendants <- (demeSizes[,as.character(Date)]*r)%*%migrationMatrix
+    descendantDates <- as.character(Date + rnorm(sum(descendants),generationTime,generationTimeSD))
+    demes <- rep(colnames(descendants),descendants)
+    if (length(descendantDates>0)) {
+      for (i in 1:length(descendantDates))
+        {
+        demeSizes[demes[i],descendantDates[i]] <- demeSizes[demes[i],descendantDates[i]] + 1
+        }
+      }
+  }
+demeSizes  
 }

7d65e048603a0442a58cf4322a18108e25424499
laurebaugnies <laure.baugnies@gmail.com>
2015-03-03 17:22:57 +0100

    Creation of the class Model, the function validity needs the complete list of the model functions

---
+++ ClassModel.R
@@ -0,0 +1,24 @@
+#Class of Model
+
+source("ClassParamModel.R")
+
+setClass(
+  #exemple of creation: 
+  #new(Class="Model", type_model="paul", param_model = new(Class="ParamModel", type_prior="julie", param_prior=c(0,1,2,3)))
+  Class = "Model",
+  representation = representation(
+    type_model = "character",
+    param_model = "ParamModel"
+  ),
+  prototype = prototype(
+    type_model = "vide", 
+    param_model = NULL
+  )
+  #validity = function(object){ ## object : type_model doesn't match any known function
+  #if (object@type_model == "vide" || autre types de fonctions){
+  #  return(FALSE)
+  #} else {
+  #  return(TRUE)
+  #}
+  #}
+)

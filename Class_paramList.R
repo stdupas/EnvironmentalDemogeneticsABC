23ad2dd2e34077b5c0b7867a7428a318e74f3f1e
laurebaugnies <laure.baugnies@gmail.com>
2015-03-06 13:36:53 +0100

    The function saveParamList and loadParamList are now functional

--- Class_paramList.R
+++ Class_paramList.R
@@ -14,3 +14,21 @@ setClass(
     	# add verification if needed
     }
 )
+
+#Function to save the ParamList
+setGeneric("saveParamList",
+           function(object, file){standardGeneric("saveParamList")})
+
+setMethod("saveParamList", "ParamList",
+          function(object, file){
+            saveRDS(object, file)
+          }
+)
+
+
+#Function to load the ParamList
+loadParamList = function(file){
+            object = readRDS(file)
+            return(object)
+}
+

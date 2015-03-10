d644a6256635787a29809f8014a6a1ac69fcd50d
laurebaugnies <laure.baugnies@gmail.com>
2015-03-10 11:41:49 +0100

    Added a getParamMethod function that returns the method used and changed the setType_prior function in order to chack if the method used allows the user to change the prior function

--- Class_paramModel.R
+++ Class_paramModel.R
@@ -1,88 +1,88 @@
 #Class of paramModel
 setClass(
-  Class = "ParamModel",
-  representation = representation(
-      method = "character",
-    name = "character",
-    type_prior = "character",
-    param_prior = "numeric",
-    param_name = "character",
-    result_prior = "numeric"
+    Class = "ParamModel",
+    representation = representation(
+        method = "character",
+        name = "character",
+        type_prior = "character",
+        param_prior = "numeric",
+        param_name = "character",
+        result_prior = "numeric"
     ),
-  prototype = prototype(
-    name = character(0),
-    type_prior = character(0), 
-    param_prior = numeric(0),
-    param_name = character(0),
-    result_prior = numeric(0)
+    prototype = prototype(
+        name = character(0),
+        type_prior = character(0), 
+        param_prior = numeric(0),
+        param_name = character(0),
+        result_prior = numeric(0)
     ),
-  validity = function(object){
-    if((is.null(object@type_prior))){
-      stop("[ ParamModel : verificatiteston ] no prior type given")
-    } else{      
-      return (TRUE)
+    validity = function(object){
+        if((is.null(object@type_prior))){
+            stop("[ ParamModel : verificatiteston ] no prior type given")
+        } else{      
+            return (TRUE)
+        }
     }
-  }
-  
+    
 )
 
 
 #Initiateur with 1 arguments
 setMethod(
-  f = "initialize",
-  signature = "ParamModel",
-  definition = function(.Object, model_num, method){
-    .Object@name=c("model:",model_num)
-    .Object@method = method
-    print("[Type 0 to quit] What function do you want to use for prior ?")
-    data_fct = read.table("functions.txt", sep = ";", header = TRUE, as.is=rep(TRUE, 4))
-    if(method == "Likelihood"){
-        possible = which(data_fct[,1]=="prior" & data_fct[,2] == "uniform")
-    } else {
-        possible = which(data_fct[,1]=="prior")
-    }
-    num = c(1:length(possible))
-    aff = rep(":", length(possible))
-    flag =0
-    while(flag ==0){
-      print(paste(num, aff,data_fct[possible,2]))    
-      scanner = as.numeric(readline())
-      if (is.na(scanner) || (scanner<0 || scanner > length(possible))){
-        print("ERROR: Your entry is incorrect, please try again")
-      }else if(scanner == 0){
-        stop("You have stopped the programm")
-      } else {
-        flag = 1
-      }      
-    }
-    .Object@type_prior = data_fct[possible[scanner],2]
-    vec = findFunctionFromFile("prior", .Object@type_prior)
-    param=NULL
-    param_name = NULL
-    for (i in 1:vec[1]){
-      flag = 0
-        while(flag == 0){
-          print(paste("What do you want for the hyper-parameter ", vec[i+1]," ?"))
-          scanner = as.numeric(readline())
-          if (is.na(scanner) || (i == 1 && scanner<0)){
-            print("ERROR: Your entry is incorrect, please try again")
-          } else {
-            flag = 1
-          }      
+    f = "initialize",
+    signature = "ParamModel",
+    definition = function(.Object, model_num, method){
+        .Object@name=c("model:",model_num)
+        .Object@method = method
+        print("[Type 0 to quit] What function do you want to use for prior ?")
+        data_fct = read.table("functions.txt", sep = ";", header = TRUE, as.is=rep(TRUE, 4))
+        if(method == "Likelihood"){
+            possible = which(data_fct[,1]=="prior" & data_fct[,2] == "uniform")
+        } else {
+            possible = which(data_fct[,1]=="prior")
         }
-      param = c(param, scanner)
-      param_name = c(param_name, vec[i+1])
+        num = c(1:length(possible))
+        aff = rep(":", length(possible))
+        flag =0
+        while(flag ==0){
+            print(paste(num, aff,data_fct[possible,2]))    
+            scanner = as.numeric(readline())
+            if (is.na(scanner) || (scanner<0 || scanner > length(possible))){
+                print("ERROR: Your entry is incorrect, please try again")
+            }else if(scanner == 0){
+                stop("You have stopped the programm")
+            } else {
+                flag = 1
+            }      
+        }
+        .Object@type_prior = data_fct[possible[scanner],2]
+        vec = findFunctionFromFile("prior", .Object@type_prior)
+        param=NULL
+        param_name = NULL
+        for (i in 1:vec[1]){
+            flag = 0
+            while(flag == 0){
+                print(paste("What do you want for the hyper-parameter ", vec[i+1]," ?"))
+                scanner = as.numeric(readline())
+                if (is.na(scanner) || (i == 1 && scanner<0)){
+                    print("ERROR: Your entry is incorrect, please try again")
+                } else {
+                    flag = 1
+                }      
+            }
+            param = c(param, scanner)
+            param_name = c(param_name, vec[i+1])
+        }
+        .Object@param_prior = param
+        .Object@param_name = param_name
+        validObject(.Object)
+        return(.Object)
     }
-    .Object@param_prior = param
-    .Object@param_name = param_name
-    validObject(.Object)
-    return(.Object)
-  }
 )
 
 #UserFriendly constructor with 1 arguments
 paramModel = function(model_num, method ){
-  new(Class = "ParamModel", model_num = model_num, method = method)
+    new(Class = "ParamModel", model_num = model_num, method = method)
 }
 
 
@@ -92,7 +92,7 @@ setGeneric("getType_prior",
 
 setMethod("getType_prior", "ParamModel",
           function(object){
-            return(object@type_prior)
+              return(object@type_prior)
           }
 )
 
@@ -102,7 +102,7 @@ setGeneric("getParam_prior",
 
 setMethod("getParam_prior", "ParamModel",
           function(object){
-            return(object@param_prior)
+              return(object@param_prior)
           }
 )
 
@@ -112,7 +112,7 @@ setGeneric("getNameParamModel",
 
 setMethod("getNameParamModel", "ParamModel",
           function(object){
-            return(object@name)
+              return(object@name)
           }
 )
 
@@ -122,7 +122,7 @@ setGeneric("getParam_prior",
 
 setMethod("getParam_prior", "ParamModel",
           function(object){
-            return(object@param_prior)
+              return(object@param_prior)
           }
 )
 
@@ -132,82 +132,96 @@ setGeneric("getResult_prior",
 
 setMethod("getResult_prior", "ParamModel",
           function(object){
-            return(object@result_prior)
+              return(object@result_prior)
+          }
+)
+
+#Function to get the "method" attribut
+setGeneric("getParamMethod",
+           function(object){standardGeneric("getParamMethod")})
+
+setMethod("getParamMethod", "ParamModel",
+          function(object){
+              return(object@method)
           }
 )
 
 
 # Function to update type_prior
 setGeneric(
-  name="setType_prior",
-  def=function(object) {standardGeneric("setType_prior")}
+    name="setType_prior",
+    def=function(object) {standardGeneric("setType_prior")}
 )
 
 setMethod(
-  f="setType_prior",
-  signature="ParamModel",
-  definition=function(object) {
-    print(paste("The actual prior function is: ", getType_prior(object)))
-    newObject = new(Class = "ParamModel", model_num = getNameParamModel(object)[2])
-    return(newObject)
-  }
+    f="setType_prior",
+    signature="ParamModel",
+    definition=function(object) {
+        if(getParamMethod(object) == "Likelihood"){
+            print("You can not change the prior type with the method: Likelihood")
+        } else {
+            print(paste("The actual prior function is: ", getType_prior(object)))
+            newObject = new(Class = "ParamModel", model_num = getNameParamModel(object)[2])
+            return(newObject)   
+        }
+    }
 )
 
 # Function to update param_prior
 setGeneric(
-  name="setParam_prior",
-  def=function(object) {standardGeneric("setParam_prior")}
+    name="setParam_prior",
+    def=function(object) {standardGeneric("setParam_prior")}
 )
 
 setMethod(
-  f="setParam_prior",
-  signature="ParamModel",
-  definition=function(object) {
-    print("The actual prior hyper-parameters are: ")
-    num = c(1:length(getParam_prior(object)))
-    cat(paste(num,":",object@param_name,"\n"))
-    flag = 0
-    while(flag == 0){
-      cat("[Type 0 to quit] Which one do you want to change?")
-      scanner = as.numeric(readline())
-      if (is.na(scanner) || (scanner>length(getParam_prior(object)) && scanner<0)){
-        print("ERROR: Your entry is incorrect, please try again")
-      } else if(scanner == 0){
-        stop("You have stopped the program")
-      }else{
-        flag = 1
-      }      
+    f="setParam_prior",
+    signature="ParamModel",
+    definition=function(object) {
+        print("The actual prior hyper-parameters are: ")
+        num = c(1:length(getParam_prior(object)))
+        cat(paste(num,":",object@param_name,"\n"))
+        flag = 0
+        while(flag == 0){
+            cat("[Type 0 to quit] Which one do you want to change?")
+            scanner = as.numeric(readline())
+            if (is.na(scanner) || (scanner>length(getParam_prior(object)) && scanner<0)){
+                print("ERROR: Your entry is incorrect, please try again")
+            } else if(scanner == 0){
+                stop("You have stopped the program")
+            }else{
+                flag = 1
+            }      
+        }
+        flag = 0
+        while(flag == 0){
+            cat("What is its new valor?")
+            new_val = as.numeric(readline())
+            if (is.na(new_val) || (scanner == 1 && new_val<=0)){
+                print("ERROR: Your entry is incorrect, please try again")
+            } else {
+                flag = 1
+            }      
+        }
+        object@param_prior[scanner] = new_val  
+        return(object)
     }
-    flag = 0
-    while(flag == 0){
-      cat("What is its new valor?")
-      new_val = as.numeric(readline())
-      if (is.na(new_val) || (scanner == 1 && new_val<=0)){
-        print("ERROR: Your entry is incorrect, please try again")
-      } else {
-        flag = 1
-      }      
-    }
-    object@param_prior[scanner] = new_val  
-    return(object)
-  }
 )
 
 
 
 findFunctionFromFile = function(model_type,fct_name){
-  data_fct = read.table("functions.txt", sep = ";", header = TRUE)
-  if(!is.element(fct_name, data_fct[,2])){
-    stop("The function: ", fct_name," does not match any knowm function")
-  } else {
-    indice = match(fct_name, data_fct[,2])
-    if(data_fct[indice,1]!=model_type){
-      stop("This function is not a ", model_type, " function")
+    data_fct = read.table("functions.txt", sep = ";", header = TRUE)
+    if(!is.element(fct_name, data_fct[,2])){
+        stop("The function: ", fct_name," does not match any knowm function")
     } else {
-      paul=unlist(strsplit((toString(data_fct[indice,4])),","))
-      return (c(data_fct[indice,3], as.vector(paul)))
+        indice = match(fct_name, data_fct[,2])
+        if(data_fct[indice,1]!=model_type){
+            stop("This function is not a ", model_type, " function")
+        } else {
+            paul=unlist(strsplit((toString(data_fct[indice,4])),","))
+            return (c(data_fct[indice,3], as.vector(paul)))
+        }
     }
-  }
 }
 
 #Function to change the Result_prior
@@ -216,8 +230,8 @@ setGeneric("setResult_prior",
 
 setMethod("setResult_prior", "ParamModel",
           function(object){
-            object@result_prior = do.call(getType_prior(object), as.list(getParam_prior(object)))
-            return(object)
+              object@result_prior = do.call(getType_prior(object), as.list(getParam_prior(object)))
+              return(object)
           }
 )
 
@@ -227,8 +241,8 @@ setGeneric("delResult_prior",
 
 setMethod("delResult_prior", "ParamModel",
           function(object){
-            object@result_prior = numeric(0)
-            return(object)
+              object@result_prior = numeric(0)
+              return(object)
           }
 )
 

32be370bd403489ad80c346cb466c846d4c1c72c
laurebaugnies <laure.baugnies@gmail.com>
2015-03-16 09:45:49 +0100

    Copy of the latest git version of script_distance_km_test.R

--- script_distance_km_test.R
+++ script_distance_km_test.R
@@ -1,12 +1,27 @@
-res = NULL
-lat = c(48.8566,49.8854,43.6046,43.2964,41.9027)
-lon = c(2.3522,2.2875,1.4442,5.3697,12.4963)
-mat = NULL
-for(i in 1:5) {
-	res = NULL
-	for(j in 1:5) {
-		res = c(res,distance(lat[i],lat[j],lon[i],lon[j]))
-	}
-	mat = rbind(mat,res)
-}
+library("argosfilter")
 
+distanceMatrixFromRaster =
+function(object){
+        # Computes a pairwise distance matrix from a raster object
+        #
+        # Args:
+        #   object: a raster object from which computes distances
+        #
+        # Returns:
+        #   A matrix of distances in meters if a coordinate system is precised
+        
+        # Extract coordinates from raster object
+        coords = xyFromCell(object = object, cell = 1:ncell(object), spatial=FALSE)
+        lat = coords[,1]
+        lon = coords[,2]
+        # Compute distance matrix
+        dist = NULL
+        for(i in 1:length(lat)) {
+            res = NULL
+            for(j in 1:length(lon)) {
+                res = c(res,distance(lat[i],lat[j],lon[i],lon[j]))
+            }
+            dist = rbind(dist,res)
+        }
+        return(dist)
+}

3a675c8195d1a640dc731613323e98bcdcf58fab
staciekith <stacie.kith@gmail.com>
2015-03-18 14:39:37 +0100

    Changed the algorithm

--- function_computeMeanEnvData.R
+++ function_computeMeanEnvData.R
@@ -2,9 +2,10 @@
 # 	nbDays: number of days
 
 computeMeanEnvData = function(data, var, nbDays) {
+	dataOld = data
 	for(d in 1:length(data[,1,var])) {
 		for(i in (nbDays+1):length(data[d,,var])) {
-			m = mean(data[d,(i-nbDays):(i-1),var])
+			m = mean(dataOld[d,(i-nbDays):(i-1),var])
 			data[d,i,var] = m
 		}
 	}

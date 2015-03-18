5e7b10547156f00f73c8ff51792684284b36f90b
staciekith <stacie.kith@gmail.com>
2015-03-18 11:51:50 +0100

    Added a function to compute the mean in x days

---
+++ function_computeMeanEnvData.R
@@ -0,0 +1,13 @@
+# Function to compute the mean of var ("pr", "tasmin", "tasmax")
+# 	nbDays: number of days
+
+computeMeanEnvData = function(data, var, nbDays) {
+	for(d in 1:length(data[,1,var])) {
+		for(i in (nbDays+1):length(data[d,,var])) {
+			m = mean(data[d,(i-nbDays):(i-1),var])
+			data[d,i,var] = m
+		}
+	}
+	return(data)
+}
+

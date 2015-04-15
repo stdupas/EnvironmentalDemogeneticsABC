a83c233f9aeaeab51888c81d51c9884192c8311d
staciekith <stacie.kith@gmail.com>
2015-04-15 10:41:38 +0200

    Added a function to aggregate dates in envData

--- function_computeMeanEnvData.R
+++ function_computeMeanEnvData.R
@@ -12,3 +12,26 @@ computeMeanEnvData = function(data, var, nbDays) {
 	return(data)
 }
 
+
+aggregateDays = function(data, nbDays, Dates) {
+	nbDates = round(length(Dates)/nbDays)-1
+	dataNew = array(0, dim=c(dim(data)[1],nbDates,dim(data)[3]))
+	# pour chaque couche
+	for(c in 1:dim(data)[3]) {
+		# pour chaque deme
+		for(d in 1:dim(data)[1]) {
+			j = 1
+			# pour toutes les nbDays dates
+			for(i in seq(nbDays,length(Dates),by=nbDays)) {
+				m = mean(data[d,(i-nbDays+1):i,c])
+				dataNew[d,j,c] = m
+				j = j+1
+			}
+		}
+	}
+	dimnames(dataNew)[1] = dimnames(data)[1]
+	dimnames(dataNew)[2] = dimnames(data[,seq(nbDays,length(Dates),by=nbDays),])[2]
+	dimnames(dataNew)[3] = dimnames(data)[3]
+
+	return(dataNew)
+}

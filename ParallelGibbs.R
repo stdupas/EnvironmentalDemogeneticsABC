7e216b7810d70ea42cf485baf47fae3ec098e63e
staciekith <stacie.kith@gmail.com>
2015-04-28 10:34:27 +0200

    FUNCTIONAL VERSION OF PARALLEL GIBBS SAMPLING

--- ParallelGibbs.R
+++ ParallelGibbs.R
@@ -1,7 +1,7 @@
 library(doParallel)
 registerDoParallel(cores=2)
 
-ParallelGibbs <- function(n=10, nbPar=8, files=FALSE) {
+ParallelGibbs <- function(n=125, nbPar=8, files=FALSE) {
     
     ##########################
     #
@@ -9,29 +9,32 @@ ParallelGibbs <- function(n=10, nbPar=8, files=FALSE) {
     #
     # Si files est TRUE, on reprend depuis les derniers parametres
     if(files == TRUE) {
-      startF = read.table(file="PARAMFROID.txt",header=TRUE)
-      startC = read.table(file="PARAMCHAUD.txt",header=TRUE)
-      nbLines = dim(startF)[1]
-      startF = startF[nbLines,]
-      startC = startC[nbLines,]
-      
-    # Sinon, on part de nouveaux parametres
+        startF = read.table(file="PARAMFROID.txt",header=TRUE)
+        startC = read.table(file="PARAMCHAUD.txt",header=TRUE)
+        nbLines = dim(startF)[1]
+        startF = startF[nbLines,]
+        startC = startC[nbLines,]
+        
+        # Sinon, on part de nouveaux parametres
     } else {
-      startF = c(4, 18, 8, 25, 4, 18, 8, 16)
-      startC = c(4, 18, 8, 25, 4, 18, 8, 16)
-      header=c("K.pr.Xmin", "K.pr.Xmax", "K.pr.Xopt", "K.pr.Yopt",
-               "R.pr.Xmin", "R.pr.Xmax", "R.pr.Xopt", "R.pr.Yopt")
-      write(header, file="PARAMFROID.txt", ncolumns=nbPar, append=FALSE)
-      write(header, file="PARAMCHAUD.txt", ncolumns=nbPar, append=FALSE)
+        startF = c(4, 18, 8, 25, 4, 18, 8, 16)
+        startC = c(4, 18, 8, 25, 4, 18, 8, 16)
+        header=c("K.pr.Xmin", "K.pr.Xmax", "K.pr.Xopt", "K.pr.Yopt",
+                 "R.pr.Xmin", "R.pr.Xmax", "R.pr.Xopt", "R.pr.Yopt")
+        write(header, file="PARAMFROID.txt", ncolumns=nbPar, append=FALSE)
+        write(header, file="PARAMCHAUD.txt", ncolumns=nbPar, append=FALSE)
+        
+        write(header, file="PARAMFROID_oneChain.txt", ncolumns=nbPar, append=FALSE)
+        write(header, file="PARAMCHAUD_oneChain.txt", ncolumns=nbPar, append=FALSE)
     }
     start = rbind(startF, startC)
-
+    
     ##########################
     #
     # NOMBRE ITERATION POUR CHAQUE CHAINE
     #
-    indiceF = 200 
-    indiceC = 200
+    indiceF = 500 
+    indiceC = 500
     indice = rbind(indiceF, indiceC)
     
     ##########################
@@ -47,7 +50,7 @@ ParallelGibbs <- function(n=10, nbPar=8, files=FALSE) {
     # THINING
     #
     thining = 2
-
+    
     ##########################
     #
     # DECLARATION DES VARIABLES A STOCKER
@@ -56,24 +59,24 @@ ParallelGibbs <- function(n=10, nbPar=8, files=FALSE) {
     ndvF = NULL
     ndpC = NULL
     ndpF = NULL
-
+    
     ##########################
     #
     # DEBUT DU GIBBS SAMPLING 
     #
     for(i in 1:n) {
-
+        
         cat(i, ": debut\n")
-
+        
         res = foreach(chaine = 1:2, .combine=c) %dopar%{
-            oneChainGibbs(start[chaine,], scale[chaine,], nbPar, indice[chaine,], thining)
+            oneChainGibbs(start[chaine,], scale[chaine,], nbPar, indice[chaine,], thining, chaine)
         }
-
+        
         postF = res[[2]]
         postC = res[[4]]
         paramF = res[[1]]
         paramC = res[[3]]
-
+        
         if(postF > postC) {
             startC = paramF
             startF = paramF
@@ -87,31 +90,31 @@ ParallelGibbs <- function(n=10, nbPar=8, files=FALSE) {
         write(startC, file="PARAMCHAUD.txt",ncolumns=8, append=TRUE)
         
         start = rbind(startF, startC)
-
+        
         ndvC = cbind(ndvC,startC)
         ndvF = cbind(ndvF,startF)
         ndpC = cbind(ndpC,postC)
         ndpF = cbind(ndpF,postF)
-
+        
         cat(i,": fin\n")
     }
     return(list(ndvC,ndpC,ndvF,ndpF))
 }
 
-oneChainGibbs <- function(start, scale, nbPar, indice, thining) {
-
+oneChainGibbs <- function(start, scale, nbPar, indice, thining, chaine) {
+    
     start0 = start
     post0 = logPostDens(start0)
     
     maxProb = post0
     maxParam = start
-
+    
     for(i in 1:indice){
         for(j in 1:nbPar){ 
             start1 = start0
             # On pioche une valeur de pas pour faire bouger les hyperparametres a partir de start0
             start1[j] = start0[j] + rnorm(1) * scale[j]
-
+            
             ##################### ConquadraticSkewed1
             ##
             ##
@@ -121,8 +124,8 @@ oneChainGibbs <- function(start, scale, nbPar, indice, thining) {
             ##
             ##
             #####################
-
-
+            
+            
             # On calcule les posteriors avec les nouveaux hyperparametres
             post1 = logPostDens(start1)
             # On decide si on garde ou non les nouvelles valeurs
@@ -133,12 +136,19 @@ oneChainGibbs <- function(start, scale, nbPar, indice, thining) {
             # Si t = 1, on garde les nouvelles valeurs, sinon on garde les anciennes valeurs
             start0[j] = start1[j] *(t==1) + start0[j] *(t==0)
             post0 = post1 * (t==1) + post0 * (t == 0)          
-
+            
             if((i%%thining == 0) && (maxProb < post0)) {
                 maxParam = start0
                 maxProb = post0
             }
         }
+        if(chaine==1) {
+            write(start0, file="PARAMFROID_oneChain.txt",ncolumns=8, append=TRUE)
+        }
+        else {
+            write(start0, file="PARAMCHAUD_oneChain.txt",ncolumns=8, append=TRUE)
+        }
+        
     }
     return(list(maxParam,maxProb))
 }
